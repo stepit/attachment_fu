18bdf7b363654af9011b4a532849346b3a640238
stefania panzanini <stefania.panzanini@gmail.com>
2012-09-20 10:11:40 +0200

    Fix setting for rails3

--- lib/technoweenie/attachment_fu.rb
+++ lib/technoweenie/attachment_fu.rb
@@ -356,8 +356,15 @@ module Technoweenie # :nodoc:
       # An array of all the tempfile objects is stored so that the Tempfile instance is held on to until
       # it's not needed anymore.  The collection is cleared after saving the attachment.
       def temp_path
+#        p = temp_paths.first
+#        p.respond_to?(:path) ? p.path : p.to_s
+        
         p = temp_paths.first
-        p.respond_to?(:path) ? p.path : p.to_s
+        if p.is_a?(ActionDispatch::Http::UploadedFile) # Rails 3 compatability fix
+          p.tempfile.path
+        else
+          p.respond_to?(:path) ? p.path : p.to_s
+        end
       end
 
       # Gets an array of the currently used temp paths.  Defaults to a copy of #full_filename.
